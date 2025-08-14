121f6caa786920374e1dd29b0e811c7e96b97fa5
noname6600 <chieng6600@gmail.com>
2025-08-14 12:24:11 +0700

    Add initial Spring Boot project

---
+++ src/main/java/com/ourwhisp/ourwhisp_backend/OurwhispBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ourwhisp.ourwhisp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurwhispBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurwhispBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ourwhisp/ourwhisp_backend/OurwhispBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourwhisp.ourwhisp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurwhispBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
